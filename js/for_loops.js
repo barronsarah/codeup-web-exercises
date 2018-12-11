a59baf4edb6f402b4a389ca781d7f27741ecbc91
Sarah Barron <s.barron2338@gmail.com>
2018-12-11 09:07:12 -0600

    uploading completed jquery exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,32 +3,32 @@
 
 //MULTIPLICATION TABLE EXERCISE 2:
 
-// function showMultiplicationTable(number){
-//     for(var i = 1; i < 11; i++) {
-//         var total = number * i;
-//         console.log(number + ' x ' + i + ' = ' + total)
-//     }
-// }
-// showMultiplicationTable(7);
+function showMultiplicationTable(number){
+    for(var i = 1; i < 11; i++) {
+        var total = number * i;
+        console.log(number + ' x ' + i + ' = ' + total)
+    }
+}
+showMultiplicationTable(7);
 
 
 // FOR LOOP EXERCISE 3:
-// var timeToStop = 10;
-//
-// for (var i = 1; i < 11; i++) {
-//     var randomNumber = Math.floor(Math.random() * 201) + 20;
-//     if (i === timeToStop) {
-//         console.log('This is your stopping point. We\'ve logged all 10 random numbers.');
-//         break;
-//     } else {
-//         if (randomNumber % 2 === 0) {
-//             console.log(randomNumber + ' is an even number');
-//
-//         } else {
-//             console.log(randomNumber + ' is an odd number');
-//         }
-//     }
-// }
+var timeToStop = 10;
+
+for (var i = 1; i < 11; i++) {
+    var randomNumber = Math.floor(Math.random() * 201) + 20;
+    if (i === timeToStop) {
+        console.log('This is your stopping point. We\'ve logged all 10 random numbers.');
+        break;
+    } else {
+        if (randomNumber % 2 === 0) {
+            console.log(randomNumber + ' is an even number');
+
+        } else {
+            console.log(randomNumber + ' is an odd number');
+        }
+    }
+}
 //
 // FOR LOOP EXERCISE 4:
 
