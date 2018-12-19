9fb46e046efef93a03bd0bb5979008c4ff196331
Sarah Barron <s.barron2338@gmail.com>
2018-12-19 12:25:05 -0600

    uploading promises exercise w/o api key

--- js/promises.js
+++ js/promises.js
@@ -1,25 +1,25 @@
 "use strict";
 
 // Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
-
-function wait(number){
-  return new Promise((resolve, reject) => {
-    if (number > 0) {
-      setTimeout(function () {
-        console.log("You'll see this after " + number + " seconds!")
-      },number *1000);
-      resolve();
-    } else {
-      reject(console.log("This rejected!"));
-    }
-  });
-}
-
-wait(2);
-wait(5);
+//
+// function wait(number){
+//   return new Promise((resolve, reject) => {
+//     if (number > 0) {
+//       setTimeout(function () {
+//         console.log("You'll see this after " + number + " seconds!")
+//       },number *1000);
+//       resolve();
+//     } else {
+//       reject(console.log("This rejected!"));
+//     }
+//   });
+// }
+//
+// wait(2);
+// wait(5);
 
 //---------------SECOND PART OF EXERCISE ---------
-
+// -------- DAY 1 ---------
 // const gitHubPromise = fetch("https://api.github.com/users", {headers: {'Authorization': 'token 5e11f8e5e76dc90d282e049066fe5825a8e8e278'}});
 //
 // const userCommits = new Promise((resolve, reject) => {
@@ -34,44 +34,102 @@ wait(5);
 // gitHubPromise.then(response => console.log(response));
 // gitHubPromise.catch(error => console.error(error));
 
-//----second try----
+//----second try--day 1--
 
-const gitHubPromise = fetch("https://api.github.com/users/barronsarah", {headers: {'Authorization': 'token 5e11f8e5e76dc90d282e049066fe5825a8e8e278'}})
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+// const gitHubPromise = fetch("https://api.github.com/users/barronsarah", {headers: {'Authorization': 'token 5e11f8e5e76dc90d282e049066fe5825a8e8e278'}})
+//   .then(
+//     function(response) {
+//       if (response.status !== 200) {
+//         console.log('Looks like there was a problem. Status Code: ' +
+//           response.status);
+//         return;
+//       }
+//
+//       // Examine the text in the response
+//       response.json().then(function(data) {
+//         console.log(data);
+//       });
+//     }
+//   )
+//   .catch(function(err) {
+//     console.log('Fetch Error :-S', err);
+//   });
+//
+// const repoPromise = fetch("https://api.github.com/users/barronsarah/repos", {headers: {'Authorization': 'token 5e11f8e5e76dc90d282e049066fe5825a8e8e278'}})
+//   .then(
+//     (response) => {
+//       if (response.status !== 200) {
+//         console.log('Looks like there was a problem. Status Code: ' +
+//           response.status);
+//         return;
+//       }
+//
+//       // Examine the text in the response
+//       response.json().then((data)=>
+//         console.log(data));
+//     }
+//   )
+//   .catch(function(err) {
+//     console.log('Fetch Error :-S', err);
+//   });
+//
+// gitHubPromise.then(repoPromise);
 
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+// ----THIRD REDO OF EXERCISE -- DAY 2 ---------
 
-const repoPromise = fetch("https://api.github.com/users/barronsarah/repos", {headers: {'Authorization': 'token 5e11f8e5e76dc90d282e049066fe5825a8e8e278'}})
-  .then(
-    (response) => {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
+const API_TOKEN = '';
+const gitHubUser = 'barronsarah';
+const url = `https://api.github.com/users/${gitHubUser}/events`;
+const fetchOptions = {
+  headers: {'Authorization': `token ${API_TOKEN}`}
+}
 
-      // Examine the text in the response
-      response.json().then((data)=>
-        console.log(data));
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
+const checkResponseForErrors = response => {
+  console.group('checkResponseForErrors')
+  console.log(response)
+  console.groupEnd()
+  if (response.status !== 200) {
+    return Promise.reject(response);
+  }
+  return Promise.resolve(response);
+}
 
-gitHubPromise.then(repoPromise);
+const parseResponseAsJson = response => {
+  console.group('parseResponseAsJson')
+  console.log(response)
+  console.groupEnd()
+  const jsonResponse = response.json();
+  return jsonResponse;
+};
+
+
+const getFirstElement= data => data[0];
+
+function getLastCommit(data) {
+  console.group('created_at')
+  console.log(data)
+  console.groupEnd()
+  return data.created_at;
+}
+
+function logData(data) {
+  console.group('logData')
+  console.log(data);
+  console.groupEnd();
+}
+
+
+function fetchJson(url) {
+  return fetch(url, fetchOptions)
+    .then(checkResponseForErrors)
+    .then(parseResponseAsJson)
+}
+
+
+fetchJson(url)
+  .then(getFirstElement)
+  .then(getLastCommit)
+  .then(logData);
+
+
 
